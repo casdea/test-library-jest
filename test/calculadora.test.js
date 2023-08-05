bd7aab0ae7d068bf4dbd3dd25a3a5432d7731f53
DESKTOP-KP43KU2\Gilbert Alexandre <gilbert.alexandre.eng@gmai.com>
2023-08-04 23:55:12 -0300

    Sessoes de teste

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,13 +1,16 @@
 let app = require("../app");
 
-test("Deve retornar 10 ao somar 5 + 5",() => {
-    let resultado = app.soma(5,5);
+describe("Operacoes Matematicas", () => {
+    test("Deve retornar 10 ao somar 5 + 5", () => {
+        let resultado = app.soma(5, 5);
+    
+        expect(resultado).toEqual(10);
+    })
+    
+    test("Deve retornar 10 ao multiplicar 5 x 2", () => {
+        let resultado = app.multiplica(5, 2);
+    
+        expect(resultado).toEqual(10);
+    })
+})
 
-    expect(resultado).toEqual(10);
-});
-
-test("Deve retornar 10 ao multiplicar 5 x 2",() => {
-    let resultado = app.multiplica(5,2);
-
-    expect(resultado).toEqual(10);
-});
