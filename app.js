4f7edf85f2883aa6109a50b228bc1d4f5df30ad3
DESKTOP-KP43KU2\Gilbert Alexandre <gilbert.alexandre.eng@gmai.com>
2023-08-04 23:48:54 -0300

    2o teste

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 module.exports = {
     soma : function(a, b) {
         return a + b;
+    },
+
+    multiplica: function(a,b) {
+        return a * b;
     }
+
 }
--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -5,3 +5,9 @@ test("Deve retornar 10 ao somar 5 + 5",() => {
 
     expect(resultado).toEqual(10);
 });
+
+test("Deve retornar 10 ao multiplicar 5 x 2",() => {
+    let resultado = app.multiplica(5,2);
+
+    expect(resultado).toEqual(10);
+});
